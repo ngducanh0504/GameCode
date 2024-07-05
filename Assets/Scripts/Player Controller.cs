1170f0e9e8d6097a21b4a9e191badc293912fe43
ngducanh0504 <nguyenhuyobelisk0@gmail.com>
2024-07-05 16:38:35 +0700

    init project

---
+++ Assets/Scripts/Player Controller.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator ani;
+    private SpriteRenderer sr;
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    private void FixedUpdate()
+    {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+        ani.SetFloat("moveX", movement.x);
+        ani.SetFloat("moveY", movement.y);
+    }
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInput();
+    }
+}
